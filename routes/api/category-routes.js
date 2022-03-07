3c585a45d95658db189056bb4af06a6e54e1d1d5
omcewan <orlandomcewan231@gmail.com>
2022-03-06 22:24:32 -0500

    update post and put routes for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,10 +44,35 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  })
+    .then((categoryData) => {
+      res.json(categoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    { category_name: req.body.category_name },
+    { where: { id: req.params.id } }
+  )
+    .then((categoryData) => {
+      if (!categoryData) {
+        console.log({ message: 'No Category with that ID was found!' });
+        return;
+      }
+      res.json(categoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
