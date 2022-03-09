05556b6111f67a53b3beb367cc9da8b8b8ce5b2a
Orlando McEwan <92196574+omcewan@users.noreply.github.com>
2022-03-09 09:38:09 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -5,6 +5,7 @@ const Sequelize = require('sequelize');
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      // If connection error occurs change host from 'localhost' to your IP address
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
